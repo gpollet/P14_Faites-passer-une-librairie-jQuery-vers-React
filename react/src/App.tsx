8bc7c29406379a8c4f5b05bf0bdd77f14421bfbe
gpollet <101146331+gpollet@users.noreply.github.com>
2023-06-14 19:43:52 +0200

    Removed unused dependencies

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router-dom"
 import "./style/app.css";
 import "normalize.css"
-//import { MantineProvider } from "@mantine/core";
 
 function App() {
   return (
     <>
-    {/*<MantineProvider
-    withGlobalStyles
-    withNormalizeCSS>*/}
     <Outlet />
-    {/*</MantineProvider>*/}
     </>
   )
 }
